9a1e73399af5b5829c31bff09eb9b2bde1cbab67
Lance Poore <linuxsociety@gmail.com>
2012-08-11 23:32:56 -0500

    Fix DC Extensions for previous commit

--- drivers/video/tegra/dc/ext/dev.c
+++ drivers/video/tegra/dc/ext/dev.c
@@ -300,25 +300,16 @@ static int lock_windows_for_flip(struct tegra_dc_ext_user *user,
 				 struct tegra_dc_ext_flip *args)
 {
 	struct tegra_dc_ext *ext = user->ext;
-	u8 idx_mask = 0;
 	int i;
 
 	for (i = 0; i < DC_N_WINDOWS; i++) {
 		int index = args->win[i].index;
+		struct tegra_dc_ext_win *win;
 
 		if (index < 0)
 			continue;
 
-		idx_mask |= BIT(index);
-	}
-
-	for (i = 0; i < DC_N_WINDOWS; i++) {
-		struct tegra_dc_ext_win *win;
-
-		if (!(idx_mask & BIT(i)))
-			continue;
-
-		win = &ext->win[i];
+		win = &ext->win[index];
 
 		mutex_lock(&win->lock);
 
@@ -330,10 +321,12 @@ static int lock_windows_for_flip(struct tegra_dc_ext_user *user,
 
 fail_unlock:
 	do {
-		if (!(idx_mask & BIT(i)))
+		int index = args->win[i].index;
+
+		if (index < 0)
 			continue;
 
-		mutex_unlock(&ext->win[i].lock);
+		mutex_unlock(&ext->win[index].lock);
 	} while (i--);
 
 	return -EACCES;
@@ -343,7 +336,6 @@ static void unlock_windows_for_flip(struct tegra_dc_ext_user *user,
 				    struct tegra_dc_ext_flip *args)
 {
 	struct tegra_dc_ext *ext = user->ext;
-	u8 idx_mask = 0;
 	int i;
 
 	for (i = 0; i < DC_N_WINDOWS; i++) {
@@ -352,14 +344,7 @@ static void unlock_windows_for_flip(struct tegra_dc_ext_user *user,
 		if (index < 0)
 			continue;
 
-		idx_mask |= BIT(index);
-	}
-
-	for (i = DC_N_WINDOWS - 1; i >= 0; i--) {
-		if (!(idx_mask & BIT(i)))
-			continue;
-
-		mutex_unlock(&ext->win[i].lock);
+		mutex_unlock(&ext->win[index].lock);
 	}
 }
 
